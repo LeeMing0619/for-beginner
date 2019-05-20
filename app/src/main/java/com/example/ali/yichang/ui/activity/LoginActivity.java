3d25f781f141a117bbba4c44b5c5e22781590f93
LeeMing <leeming0619@hotmail.com>
2019-05-19 19:03:00 -0700

    sd
    a

---
+++ app/src/main/java/com/example/ali/yichang/ui/activity/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.ali.yichang.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ali.yichang.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
